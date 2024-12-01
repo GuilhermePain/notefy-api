591cbcff9addbbcacb2de667ceaec3ce883d6db0
GuilhermePain <guizaorocha543@gmail.com>
2024-12-01 16:02:36 -0300

    Configurando arquivos para inicializar a api hospedada

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT
   app.useGlobalPipes(new ValidationPipe);
   app.enableCors({
     origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true
   });
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
