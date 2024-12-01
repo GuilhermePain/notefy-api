65b8a77c5018834288e088a8e8c7212ca98a8e5c
GuilhermePain <guizaorocha543@gmail.com>
2024-12-01 16:21:10 -0300

    Configurando cors na api

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT
   app.useGlobalPipes(new ValidationPipe);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'conservative-violette-guilhermerocha-4c0b4e6a.koyeb.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true
   });
